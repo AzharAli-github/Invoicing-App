d1576421995bf74a2cb1f3d9da9051ef340f9cfb
AzharAli-github <azharborithic11@gmail.com>
2026-01-11 00:46:29 +0500

    APP

---
+++ invoice-master/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Flex } from "@/components/app/directional/Flex";
+import { Text } from "@/components/app/Text";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Flex
+      as={"main"}
+      flexDirection="column"
+      gap={12}
+      alignItems="center"
+      justifyContent="center"
+      className="h-screen w-screen"
+    >
+      <Text as="h1">Invoice App</Text>
+      <Button asChild>
+        <Link href={"/dashboard"}>Dashboard</Link>
+      </Button>
+    </Flex>
+  );
+}
