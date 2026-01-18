53e8841eaf59ae85c9f36b183f6ca8dec8e1eddc
AzharAli-github <azharborithic11@gmail.com>
2026-01-18 20:51:01 +0500

    Layout

---
+++ invoice-master/src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@/components/app/directional/Flex";
+import React, { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <Flex
+      as={"section"}
+      flexDirection={"column"}
+      justifyContent={"flex-start"}
+      gap={12}
+      alignItems={"center"}
+      className="w-screen h-screen py-8"
+    >
+      {children}
+    </Flex>
+  );
+}
